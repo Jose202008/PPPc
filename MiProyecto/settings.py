04f2d728cdfdf1a708795d3136a795ad604f06b0
Jose Duran <setonoan@gmail.com>
2020-10-16 00:09:40 -0300

    Casi listo para subir a Producción

--- MiProyecto/settings.py
+++ MiProyecto/settings.py
@@ -19,10 +19,12 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vyi1-vnm8!)2jbin7f)i$d(zqkm#%tx8+n8n+jd51u!iefvfyr'
+# SECRET_KEY = 'vyi1-vnm8!)2jbin7f)i$d(zqkm#%tx8+n8n+jd51u!iefvfyr'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'vyi1-vnm8!)2jbin7f)i$d(zqkm#%tx8+n8n+jd51u!iefvfyr')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = []
 
